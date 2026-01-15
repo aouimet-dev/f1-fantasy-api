511d5f49ff9fb4cf39516bf818deae418582917a
Alexandre Ouimet <aouimet.dev@gmail.com>
2026-01-15 09:02:29 -0500

    Easter egg

--- src/main/java/com/ouimet/f1/fantasy_service/controller/F1Controller.java
+++ src/main/java/com/ouimet/f1/fantasy_service/controller/F1Controller.java
@@ -1,10 +1,12 @@
 package com.ouimet.f1.fantasy_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
 @RequestMapping("/api/f1")
@@ -12,4 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class F1Controller {
 
+    @GetMapping
+    public String home() {
+        return "Si Vince ne m'aide pas, c'est une petite salope! :D";
+    }
+    
 }
